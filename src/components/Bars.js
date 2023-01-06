3b8cb3fb54827b82ce6990fa0d14be850d9cec15
solomwaura <solomonmicro@gmail.com>
2023-01-06 09:22:17 +0300

    progress

--- src/components/Bars.js
+++ src/components/Bars.js
@@ -6,16 +6,21 @@ function Bars(index, length, color) {
 
     const [len, setLen] = useState(length)
 
-    const barStyle = {
+    // const barStyle = {
         
-        height: length
-    }
+    //     height: length
+    // }
+    const colors = [['rgba(161,90,241,0.5)','rgba(61,220,241,0.2)'],
+                    ['rgba(255,48,79,1)','rgba(255,48,79,0.5)'],
+                    ['rgba(131,232,90,1)','rgba(255,48,79,0.2)'],];
+
     const inputStyle ={
         position: 'relative',
         width: length,
-        top: Math.floor(length/2) -10,
-        left: Math.floor(length/2)+10,
-        border: 'none'
+        top: Math.floor(length/2) - 12,
+        left: Math.floor(length/2) + 13,
+        border: 'none',
+        background: 'none'
         
     }
     const front_bottom = {
@@ -24,19 +29,21 @@ function Bars(index, length, color) {
         boxShadow: '5px 5px 50px 5px ${colors[color][1]}',
         transition: '0.3s'
     }
-    const right_left = {
+    const front_right_left = {
         transform: 'translateY( ${200 - length}px)',
         backgroundColor: '${colors[color][0]}',
         boxShadow: '5px 5px 50px 5px ${colors[color][1]}',
         transition: '0.3s'
     }
-    const colors = [['rgba(61,90,241,0.5)','rgba(61,90,241,0.2)'],
-                    ['rgba(255,48,79,1)','rgba(255,48,79,0.5)'],
-                    ['rgba(131,232,90,1)','rgba(255,48,79,0.2)'],];
+    
+    const quantity ={
+        position:'relative',
+        top: 225,
+    }
     
     const handleChange = (e) =>{
         let val = e.target.value;
-        if(val == ''){
+        if(val === ''){
             setLen(0)
 
         }else{
@@ -51,29 +58,38 @@ function Bars(index, length, color) {
     }
   return (
     <>
-        <div className="bar" style={barStyle}>
+        <div className="bar">
             <div className="side top">
 
             </div>
             <div className="side bottom" style={front_bottom}></div>
-            <div className="side right" style={right_left}>
-                <div className="color-bar right-color-bar" style={right_left} ></div>
+            <div className="side right" style={front_right_left}>
+                <div className="color-bar right-color-bar" style={front_right_left} ></div>
             </div>
-            <div className="side left" style={right_left}>
-            <div className="color-bar left-color-bar" style={right_left} ></div>
+            <div className="side left" style={front_right_left}>
+            <div className="color-bar left-color-bar" style={front_right_left} ></div>
             </div>
             <div className="side front" style={front_bottom}>
-                <div className="color-bar front-color-bar" style={front_bottom}></div>
+                <div className="color-bar front-color-bar" style={front_bottom}>
                 <input
                  type="number" 
                  length={length}
                   value={len} 
                   className='input'
                 onChange={handleChange} style={inputStyle} />
+                </div>
+                
             </div>
             <div className="side back">
-                <div className="color-bar back-color-bar" style={front_bottom}>
+                <div className="color-bar back-color-bar" style={front_bottom}> </div>
 
+            </div>
+            <div className="quantity-nav">
+                <div className="quantity-button quantity-up" style={quantity}>
+                    +
+                </div>
+                <div className="quantity-button quantity-down" style={quantity}>
+                    -
                 </div>
             </div>
         </div>
